366f620c8e014e97770cd463a16b365e940de512
Yesser <benselmayasser1@gmail.com>
2024-07-31 14:15:08 +0100

    methode pour afficher le pdf mais dans un nouvel onglet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
-import { UserTypeComponent } from './user-type/user-type.component';
+import { UserTypeComponent } from './components/user-type/user-type.component';
+import { PdfViewerComponent } from './pdf-viewer/pdf-viewer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UserTypeComponent,
+    PdfViewerComponent,
   ],
   imports: [
     BrowserModule,
